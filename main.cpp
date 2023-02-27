cfecf29072afd13989a8ccb3c41489b840165bbf
josh <jm8hjkl@gmail.com>
2023-02-27 13:56:30 -0500

    add stop when not going anywhere

--- main.cpp
+++ main.cpp
@@ -26,18 +26,38 @@ const int ONE_REVOLUTION_COUNTS = 318;
 
 struct Gui {
     void textLine(std::string s, int row) {
-        LCD.WriteRC((s + "                 ").c_str(), row, 0);
+        int width = 26;
+        if (s.length() < width) {
+            s.append(width - s.length(), ' ');
+        }
+        LCD.WriteRC(s.c_str(), row, 0);
+    }
+
+    void textLine(std::string s, double value, int row) {
+        textLine((s + ": " + std::to_string(value)), row);
     }
 };
 
 Gui gui;
 
+void wait_for_start_light() {
+    double nextTime = 0;
+    gui.textLine("waiting for light", 0);
+    while (cdsCell.Value() <= 0.2 || cdsCell.Value() >= 1.5) {
+        if (nextTime > TimeNow()) {
+            gui.textLine("cds", cdsCell.Value(), 1);
+            nextTime = TimeNow() + .1;
+        }
+    }
+}
+
+const double TIME_OUT = 10.0;
+
 void move_forward(int percent, double inches)
 {
-    double timeOut = TimeNow() + 10.0;
+    LCD.Clear();
+    double startTime = TimeNow();
     int expectedCounts = (ONE_REVOLUTION_COUNTS * inches) / (2 * PI * WHEEL_RADIUS);
-    LCD.Write("Expected counts ");
-    LCD.WriteLine(expectedCounts);
 
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
@@ -45,18 +65,30 @@ void move_forward(int percent, double inches)
     right_motor.SetPercent(percent);
     left_motor.SetPercent(leftMultiplier*percent);
 
-    double nextWriteLineTime = 0;
+    double nextTime = 0;
+    int oldCounts = -10;
+    int numberZeros = 0;
     while(true) {
-        if (TimeNow() > timeOut) {
+        if (TimeNow() > startTime+TIME_OUT) {
             break;
         }
         int counts = (left_encoder.Counts() + right_encoder.Counts()) / 2.;
         if (counts > expectedCounts) {
             break;
         }
-        if(TimeNow() > nextWriteLineTime) {
-            // LCD.WriteLine(counts);
-            nextWriteLineTime = TimeNow()+.1;
+        if (counts == oldCounts) {
+            numberZeros++;
+        }
+        if (numberZeros > 4) {
+            break;
+        }
+        gui.textLine("expected counts", expectedCounts, 4);
+        if (TimeNow() > nextTime) {
+            gui.textLine("counts", counts, 1);
+            gui.textLine("distance", ((double)counts / expectedCounts) * inches, 2);
+            oldCounts = counts;
+            gui.textLine("time", TimeNow() - startTime, 3);
+            nextTime = TimeNow() + .25;
         }
     }
 
@@ -67,6 +99,7 @@ void move_forward(int percent, double inches)
 
 void turn_left(int percent, double degrees)
 {
+    LCD.Clear();
     gui.textLine("turn left", 0);
     int counts = 222 * (degrees/90);
     right_encoder.ResetCounts();
@@ -75,6 +108,7 @@ void turn_left(int percent, double degrees)
     right_motor.SetPercent(-percent);
     left_motor.SetPercent(leftMultiplier*percent);
 
+    double nextTime = 0;
     while((left_encoder.Counts()+right_encoder.Counts())/2. < counts);
 
     right_motor.Stop();
@@ -84,6 +118,7 @@ void turn_left(int percent, double degrees)
 
 void turn_right(int percent, double degrees)
 {
+    LCD.Clear();
     gui.textLine("turn right", 0);
     int counts = 222 * (degrees/90);
     right_encoder.ResetCounts();
@@ -99,20 +134,30 @@ void turn_right(int percent, double degrees)
 }
 
 void try_course() {
+    wait_for_start_light();
+
     // go toward ramp
     move_forward(25, 14.0);
     // go up ramp
     move_forward(40, 5.0+12.31+4.0);
 
-    // go to kiosk
+    // align with kiosk
     turn_left(25, 90);
     move_forward(25, 8.8);
     turn_right(25, 90);
+
+    // go to kisok
     move_forward(25, 30);
+
+    // move back
     move_forward(-25, 4);
+
+    // align with ramp
     turn_left(25, 90);
     move_forward(25, 11);
     turn_left(25, 90);
+
+    // go down ramp
     move_forward(25, 30);
 }
 
@@ -134,14 +179,15 @@ int main(void)
     LCD.SetFontColor(WHITE);
 
 
-    LCD.WriteLine("Go up ramp test");
-    LCD.WriteLine("Touch the screen");
-    while(!LCD.Touch(&touchX,&touchY)); //Wait for screen to be pressed
+    gui.textLine("Touch the screen", 0);
+    while(!LCD.Touch(&touchX,&touchY)) {
+        gui.textLine("cds", cdsCell.Value(), 1);
+        Sleep(.1);
+    }; //Wait for screen to be pressed
     while(LCD.Touch(&touchX,&touchY)); //Wait for screen to be unpressed
-        
-    // calibrate_motors();
-    while (cdsCell.Value() <= 0.2 || cdsCell.Value() >= 1.5) {
-    }
+    LCD.Clear();
+
+    // move_forward(25, 8);
     try_course();
 
     return 0;
