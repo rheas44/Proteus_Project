d7733670dccd1dbfedff104733677ff573c77ca0
josh <jm8hjkl@gmail.com>
2023-02-28 14:45:10 -0500

    use only left encoder; add time out to turning

--- main.cpp
+++ main.cpp
@@ -3,22 +3,22 @@
 #include <FEHUtility.h>
 #include <FEHMotor.h>
 #include <FEHRPS.h>
+#include <cmath>
 #include <vector>
 #include <string>
 
 const int LCD_WIDTH = 320;
 const int LCD_HEIGHT = 240;
 
-double leftMultiplier = 1.009;
+double leftMultiplier = 1;
 
 //Declarations for encoders & motors
-DigitalEncoder right_encoder(FEHIO::P0_0);
 DigitalEncoder left_encoder(FEHIO::P0_1);
 FEHMotor right_motor(FEHMotor::Motor0,9.0);
 FEHMotor left_motor(FEHMotor::Motor1,9.0);
 
 //Declaration for analog input pin
-AnalogInputPin cdsCell(FEHIO::P1_1);
+AnalogInputPin cdsCell(FEHIO::P3_7);
 
 const double WHEEL_RADIUS = 2.5 / 2;
 const double PI = 3.14159;
@@ -40,12 +40,21 @@ struct Gui {
 
 Gui gui;
 
+void resetCounts() {
+    left_encoder.ResetCounts();
+}
+
+double getCounts() {
+    return left_encoder.Counts();
+}
+
 void wait_for_start_light() {
     double nextTime = 0;
     gui.textLine("waiting for light", 0);
-    while (cdsCell.Value() <= 0.2 || cdsCell.Value() >= 1.5) {
-        if (nextTime > TimeNow()) {
-            gui.textLine("cds", cdsCell.Value(), 1);
+    while (cdsCell.Value() >= 1.5) {
+        if (TimeNow() > nextTime) {
+            gui.textLine("cds: ", cdsCell.Value(), 1);
+            gui.textLine("expected: ", 1.5, 2);
             nextTime = TimeNow() + .1;
         }
     }
@@ -59,8 +68,7 @@ void move_forward(int percent, double inches)
     double startTime = TimeNow();
     int expectedCounts = (ONE_REVOLUTION_COUNTS * inches) / (2 * PI * WHEEL_RADIUS);
 
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
+    resetCounts();
 
     right_motor.SetPercent(percent);
     left_motor.SetPercent(leftMultiplier*percent);
@@ -72,7 +80,7 @@ void move_forward(int percent, double inches)
         if (TimeNow() > startTime+TIME_OUT) {
             break;
         }
-        int counts = (left_encoder.Counts() + right_encoder.Counts()) / 2.;
+        int counts = getCounts();
         if (counts > expectedCounts) {
             break;
         }
@@ -97,45 +105,43 @@ void move_forward(int percent, double inches)
 }
 
 
-void turn_left(int percent, double degrees)
-{
-    LCD.Clear();
-    gui.textLine("turn left", 0);
-    int counts = 222 * (degrees/90);
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
-
-    right_motor.SetPercent(-percent);
-    left_motor.SetPercent(leftMultiplier*percent);
-
-    double nextTime = 0;
-    while((left_encoder.Counts()+right_encoder.Counts())/2. < counts);
-
-    right_motor.Stop();
-    left_motor.Stop();
-}
-
-
 void turn_right(int percent, double degrees)
 {
     LCD.Clear();
-    gui.textLine("turn right", 0);
-    int counts = 222 * (degrees/90);
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
+    gui.textLine(percent < 0 ? "turn left" : "turn right", 0);
+    int expectedCounts = std::abs(222 * (degrees/90));
+    resetCounts();
 
     right_motor.SetPercent(percent);
     left_motor.SetPercent(-leftMultiplier*percent);
 
-    while((left_encoder.Counts()+right_encoder.Counts())/2. < counts);
+    double nextTime = 0;
+    double startTime = TimeNow();
+    while(true) {
+        int counts = getCounts();
+        if (TimeNow() > nextTime) {
+            gui.textLine("counts", counts, 1);
+            gui.textLine("expected", expectedCounts, 2);
+            gui.textLine("time", TimeNow() - startTime, 2);
+            if (TimeNow() > startTime + TIME_OUT) {
+                break;
+            }
+            if (counts >= expectedCounts) {
+                break;
+            }
+        }
+    }
 
     right_motor.Stop();
     left_motor.Stop();
 }
 
-void try_course() {
-    // wait_for_start_light();
+void turn_left(int percent, double degrees)
+{
+    turn_right(-percent, degrees);
+}
 
+void try_course() {
     // go forward
     move_forward(25, 8);
 
@@ -173,9 +179,10 @@ void try_course() {
 }
 
 void calibrate_motors() {
+    DigitalEncoder right_encoder(FEHIO::P0_0);
     leftMultiplier = 1.0;
     LCD.WriteLine("Calibrating motors");
-    move_forward(25, 8.0);
+    move_forward(25, 8);
     LCD.Clear();
     LCD.Write("Left: ");
     LCD.WriteLine(left_encoder.Counts());
@@ -200,9 +207,14 @@ int main(void)
     while(LCD.Touch(&touchX,&touchY)); //Wait for screen to be unpressed
     LCD.Clear();
 
+    left_motor.SetPercent(25);
+    right_motor.SetPercent(25);
+
     // move_forward(25, 8);
-    // calibrate_motors();
-    try_course();
+    calibrate_motors();
+
+    // wait_for_start_light()
+    // try_course();
 
     return 0;
 }
